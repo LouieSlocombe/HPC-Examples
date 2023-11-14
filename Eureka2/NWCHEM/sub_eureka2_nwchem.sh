fb06901d922682cc9a21113d30890b47171d4476
Louie <louies@hotmail.co.uk>
2023-11-14 12:01:08 +0000

    Added Eureka2 examples

---
+++ Eureka2/NWCHEM/sub_eureka2_nwchem.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#SBATCH --partition=shared
+#SBATCH --job-name=ase
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00-01:00:00 ## 07-00:00:00 00-01:00:00
+#SBATCH -o slurm.%N.%j.out 
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --exclusive
+
+cd $SLURM_SUBMIT_DIR
+echo $SLURM_NODELIST
+
+echo "Starting at $(date)"
+echo "Job submitted to the ${SLURM_JOB_PARTITION} partition"
+echo "Job name: ${SLURM_JOB_NAME}, Job ID: ${SLURM_JOB_ID}"
+echo "I have ${SLURM_CPUS_ON_NODE} CPUs on compute node $(hostname)"
+echo $SLURM_NODELIST
+
+module load GCC/11.2.0  
+module load OpenMPI/4.1.1
+module load nwchem/7.0.2-gcc 
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK 
+export OMP_PLACES=cores
+
+echo "Starting calculation at $(date)"
+SECONDS=0
+
+mpirun -np $SLURM_NTASKS nwchem water.inp > water.out
+
+duration=$SECONDS
+echo "Calculation ended at $(date)"
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+exit
+
+
