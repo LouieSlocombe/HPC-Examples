b496181a4d161834ec716fe68aa44b8ae2feca7d
Louie <louies@hotmail.co.uk>
2023-11-14 12:11:11 +0000

    added more files

---
+++ Eureka2/ASE/README.md
@@ -0,0 +1,2 @@
+# How to submit the job
+Upload the files into a new directory on parallel scratch. Then change directory to this directory using "cd parallel_scratch/test"
---
+++ Eureka2/ASE/ase_nwchem.py
@@ -0,0 +1,19 @@
+# Example: structure optimization of hydrogen molecule
+from ase import Atoms
+from ase.optimize import BFGS
+from ase.calculators.nwchem import NWChem
+from ase.io import write
+from ase.build import molecule
+from ase.io import read
+
+atoms = molecule('H2O')
+
+calc = NWChem(label='calc/nwchem',
+              dft=dict(maxiter=100,
+                       xc='B3LYP'),
+              basis='6-31+G*')
+
+atoms.calc = calc
+
+opt = BFGS(atoms, trajectory='H2O.traj', logfile='H2O.log')
+opt.run(fmax=0.01)
